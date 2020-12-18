f8a88e4c5d3b7db070669b4ddca426bf9492cb85
infomuscle <infomuscle10@gmail.com>
2020-12-18 20:43:41 +0900

    Lesson5 - GenomicRangeQuery Timeout

--- codility-java/src/GenomicRangeQuery.java
+++ codility-java/src/GenomicRangeQuery.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GenomicRangeQuery {
 
@@ -10,29 +13,20 @@ public class GenomicRangeQuery {
 
             int[] answer = new int[P.length];
 
-            Map<Character, Integer> factorMap = new HashMap<>();
-            factorMap.put('A', 1);
-            factorMap.put('C', 2);
-            factorMap.put('G', 3);
-            factorMap.put('T', 4);
+            Map<String, Integer> factorMap = new LinkedHashMap<>();
+            factorMap.put("A", 1);
+            factorMap.put("C", 2);
+            factorMap.put("G", 3);
+            factorMap.put("T", 4);
 
-            Map<Character, List<Integer>> indexMap = new LinkedHashMap<>();
-            indexMap.put('T', new ArrayList<Integer>());
-            indexMap.put('G', new ArrayList<Integer>());
-            indexMap.put('C', new ArrayList<Integer>());
-            indexMap.put('A', new ArrayList<Integer>());
-
-            for (int i = 0; i < N; i++) {
-                indexMap.get(S.charAt(i)).add(i);
-            }
-
+            String[] split = S.split("");
             for (int i = 0; i < M; i++) {
-                for (Character key : indexMap.keySet()) {
-                    for (Integer idx : indexMap.get(key)) {
-                        if (idx >= P[i] && idx <= Q[i]) {
-                            answer[i] = factorMap.get(key);
-                            break;
-                        }
+                List<String> sp = Arrays.asList(Arrays.copyOfRange(split, P[i], Q[i] + 1));
+                System.out.println(sp);
+                for (String key : factorMap.keySet()) {
+                    if (sp.contains(key)) {
+                        answer[i] = factorMap.get(key);
+                        break;
                     }
                 }
             }
