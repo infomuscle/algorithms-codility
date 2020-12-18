e6c1d9dec9364ee3bc778fbe335f189da592364e
infomuscle <infomuscle10@gmail.com>
2020-12-18 20:15:47 +0900

    Lesson5 - GenomicRangeQuery Timeout

--- codility-java/src/GenomicRangeQuery.java
+++ codility-java/src/GenomicRangeQuery.java
@@ -1,26 +1,40 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class GenomicRangeQuery {
 
     class Solution {
         public int[] solution(String S, int[] P, int[] Q) {
 
+            int N = S.length();
+            int M = P.length;
+
             int[] answer = new int[P.length];
 
-            Map<String, Integer> map = new HashMap<>();
-            map.put("A", 1);
-            map.put("C", 2);
-            map.put("G", 3);
-            map.put("T", 4);
+            Map<Character, Integer> factorMap = new HashMap<>();
+            factorMap.put('A', 1);
+            factorMap.put('C', 2);
+            factorMap.put('G', 3);
+            factorMap.put('T', 4);
 
-            for (int i = 0; i < P.length; i++) {
-                String[] queryBase = S.substring(P[i], Q[i] + 1).split("");
+            Map<Character, List<Integer>> indexMap = new LinkedHashMap<>();
+            indexMap.put('T', new ArrayList<Integer>());
+            indexMap.put('G', new ArrayList<Integer>());
+            indexMap.put('C', new ArrayList<Integer>());
+            indexMap.put('A', new ArrayList<Integer>());
 
-                Arrays.sort(queryBase);
+            for (int i = 0; i < N; i++) {
+                indexMap.get(S.charAt(i)).add(i);
+            }
 
-                answer[i] = map.get(queryBase[0]);
+            for (int i = 0; i < M; i++) {
+                for (Character key : indexMap.keySet()) {
+                    for (Integer idx : indexMap.get(key)) {
+                        if (idx >= P[i] && idx <= Q[i]) {
+                            answer[i] = factorMap.get(key);
+                            break;
+                        }
+                    }
+                }
             }
 
             return answer;
