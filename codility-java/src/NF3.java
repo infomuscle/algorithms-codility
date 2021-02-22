a53e076be11ad4ddf2f0d1d3fa8b87a020572d36
infomuscle <infomuscle10@gmail.com>
2021-02-22 21:25:13 +0900

    Added Comments and Test Cases for NF3

--- codility-java/src/NF3.java
+++ codility-java/src/NF3.java
@@ -6,20 +6,29 @@ public class NF3 {
 
     class Solution {
         public int solution(int[] A) {
+
+            // A를 오름차순으로 정렬한다.
             Arrays.sort(A);
+
+            // 정수 Key와 불 Value 포맷의 맵을 생성한다.
             Map<Integer, Boolean> nums = new HashMap<>();
+
+            // A의 각 원소를 Key로 하여 맵에 등록한다.
             for (int i = 0; i < A.length; i++) {
                 nums.put(A[i], true);
             }
 
             int positive;
+            // A의 원소를 앞에서부터 순서대로 체크한다.
             for (int i = 0; i < A.length; i++) {
+
+                // A[i]가 음수일 경우, 절대값이 같은 양수가 맵에 있다면 양수 리턴한다.
                 if (A[i] < 0) {
                     positive = A[i] * (-1);
                     if (nums.containsKey(positive)) {
                         return positive;
                     }
-                } else {
+                } else { // A[i]가 음수가 아닐 경우, 양수와 음수를 다 갖고 있는 경우가 없으므로 바로 종료한다.
                     break;
                 }
             }
@@ -35,11 +44,13 @@ public class NF3 {
         int[] a1 = {3, 2, -2, 5, -3};
         int[] a2 = {1, 1, 2, -1, 2, -1};
         int[] a3 = {1, 2, 3, -4};
+        int[] a4 = {-1000000000, 1000000000};
+        int[] a5 = {-2};
 
         System.out.println(sol.solution(a1));
         System.out.println(sol.solution(a2));
         System.out.println(sol.solution(a3));
-
-
+        System.out.println(sol.solution(a4));
+        System.out.println(sol.solution(a5));
     }
 }
