1ece0f99eddc4172c817c2cc8ba4b9264405f7e5
infomuscle <infomuscle10@gmail.com>
2021-02-23 16:21:55 +0900

    Added Comments for NF1

--- codility-java/src/NF1.java
+++ codility-java/src/NF1.java
@@ -12,14 +12,16 @@ public class NF1 {
             int len = S[0].length();
 
             // {"문자" : {문자열 내에서의 위치: [해당 위치에 해당 문자를 갖고 있는 문자열의 인덱스]}}인 맵 생성.
+            // [“abc”, “ade”, “xyz”, "qrz"] -> {“a”: {0: [0,1]}, "z": {2: [2,3]}}
             Map<Character, Map<Integer, ArrayList<Integer>>> wordMap = new HashMap<>();
 
             // S의 각 문자열들을 처리.
             char ch;
-            for (int i = 0; i < S.length; i++) {
+            for (int i = 0; i < S.length; i++) { // S.length == N
 
                 // 문자열 S[i]의 각 문자들을 맵에 넣는다.
-                for (int j = 0; j < len; j++) {
+                // N * M <= 30000이라 시간복잡도에는 큰 이슈 없을 것.
+                for (int j = 0; j < len; j++) { // len == M
                     ch = S[i].charAt(j);
 
                     // 문자 ch가 처음 나오는 거면, Key ch의 Value로 빈 맵 생성하여 넣는다.
@@ -35,11 +37,12 @@ public class NF1 {
                     wordMap.get(ch).get(j).add(i);
                 }
             }
+            System.out.println(wordMap);
 
             // 빈 문자열 생성한다.
             int[] answer = {};
-            for (Character chKey : wordMap.keySet()) {
-                for (Integer intKey : wordMap.get(chKey).keySet()) {
+            for (Character chKey : wordMap.keySet()) { // 최대 26번
+                for (Integer intKey : wordMap.get(chKey).keySet()) { // 최대 2000(M)
                     // 문자 ch가 문자열 내 인덱스에 등장한 횟수가 2보다 클 경우 정답에 해당.
                     if (wordMap.get(chKey).get(intKey).size() >= 2) {
                         // 리스트의 크기가 2보다 크니깐 0, 1 다 집어도 예외 발생 X.
